9db641fb0224960e62817f8253b3eb7ab460aa9c
AashiiMittal <aashimittal2004@gmail.com>
2025-01-18 11:35:46 +0530

    commit

--- Main.java
+++ Main.java
@@ -10,8 +10,10 @@ public class Addition {
         int result= num1+num2;
         int result2= num1-num2;
         int result3= num1*num2;
+        int result4= num1/num2;
         System.out.println(result);
         System.out.println(result2);
         System.out.println(result3);
+        System.out.println(result4);
     }
 }
